57169c06c0339179f30ea9d9a57bd11b46fe9166
unknown <folorunsodaniel5@gmail.com>
2020-06-13 20:43:37 +0200

    design_update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-$(document).ready(function()
-{
-    
+$(document).ready(function () {
+
     $(".post").hide();
     $(".new_post").hide();
     $(".sign_up").hide();
@@ -8,80 +7,94 @@ $(document).ready(function()
     $("#sign_out").hide();
     $("#new_post").hide();
 
-    $("#home").click(function()
-    {
-        $(".posts").show();
-        $(".post").hide();
-        $(".new_post").hide();
-        $(".sign_in").hide();
-        $(".sign_up").hide();
-    });
+    $(".hamburger").click(
+        function () {
+            $(".list-items").toggle(()=>{
+                $(this).slideDown()
+            })
+        }
+    )
+    $("#home").click(
+        function () {
+            $(".list-items").toggle(()=>{
+                $(this).slideDown()
+            })
+        }
+    )
 
     $(".link a").click(
-        function()
-        {
+        function () {
             $(".post").show();
             $(".posts").hide();
         }
     );
 
     $("#sign_in").click(
-        function()
-        {
-            $(this).hide();
+        function () {
             $("#sign_out").show();
             $("#register").hide();
             $("#new_post").show();
-            $(".sign_in").show();
+            $(".sign_in").slideDown()
             $(".sign_up").hide();
             $(".new_post").hide();
             $(".post").hide();
             $(".posts").hide();
+            $(".list-items").toggle(()=>{
+                $(this).slideDown();
+                $("#sign_in").hide();
+            })
         }
     )
 
     $("#sign_out").click(
-        function()
-        {
-            $(this).hide();
+        function () {
             $("#sign_in").show();
             $("#register").show();
             $("#new_post").hide();
-            $(".sign_in").show();
+            $(".sign_in").slideDown();
             $(".sign_up").hide();
             $(".new_post").hide();
             $(".post").hide();
             $(".posts").hide();
+            $(".list-items").toggle(()=>{
+                $(this).slideDown();
+                $("#sign_out").hide();
+            })
         }
     )
 
     $("#register").click(
-        function()
-        {
+        function () {
             $(this).hide();
             $("#sign_in").show();
+            $("#sign_out").hide();
             $("#register").show();
             $("#new_post").hide();
             $(".sign_in").hide();
-            $(".sign_up").show();
+            $(".sign_up").slideDown();
             $(".new_post").hide();
             $(".post").hide();
             $(".posts").hide();
+            $(".list-items").toggle(()=>{
+                $(this).slideDown()
+            })
         }
     )
 
     $("#new_post").click(
-        function()
-        {
+        function () {
             $(this).show();
             $("#sign_out").show();
             $("#sign_in").hide();
             $("#register").hide();
             $(".sign_in").hide();
             $(".sign_up").hide();
-            $(".new_post").show();
+            $(".new_post").slideDown();
             $(".post").hide();
             $(".posts").hide();
+            $(".list-items").toggle(()=>{
+                $(this).slideDown()
+            })
         }
     )
 
@@ -89,30 +102,27 @@ $(document).ready(function()
         type: 'GET',
         dataType: 'json',
         url: 'http://localhost:800/',
-        success: (result, status, xhr) =>
-        {
-         
-            for(var i = 0; i < result.data.length; i++)
-            {
-                console.log(result.data[i].id+"\n"+result.data[i].author+"\n"+result.data[i].description+"\n"+result.data[i].full_story)
+        success: (result, status, xhr) => {
+
+            for (var i = 0; i < result.data.length; i++) {
+                console.log(result.data[i].id + "\n" + result.data[i].author + "\n" + result.data[i].description + "\n" + result.data[i].full_story)
                 $("body").append(
-                    "<div class=\"posts\">"+
+                    "<div class=\"posts\">" +
                     "<div class=\"title\">"
-                    +result.data[i].author+
-                    "</div>"+
+                    + result.data[i].author +
+                    "</div>" +
                     "<div class=\"summary\">"
-                    +result.data[i].description+
-                    "</div>"+
-                    "<div class=\"link\">"+
-                    "<a href=\"#\">Read More</a>"+
-                    "</div>"+
+                    + result.data[i].description +
+                    "</div>" +
+                    "<div class=\"link\">" +
+                    "<a href=\"#\">Read More</a>" +
+                    "</div>" +
                     "</div>"
-                    )
-    
+                )
+
             }
         },
-        error: (xhr, status, error) =>
-        {
+        error: (xhr, status, error) => {
             console.log(error)
         }
     })
@@ -122,8 +132,7 @@ $(document).ready(function()
             type: "GET",
             dataType: "json",
             url: "http://localhost:800/oyenike",
-            success: (result, xhr, status) =>
-            {
+            success: (result, xhr, status) => {
                 console.log(result.data)
 
             }
